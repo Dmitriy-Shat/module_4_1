ff4a5a0bdb00b2bd513319333809564255ea0fd6
Dmitriy-Shat <jaren@inbox.ru>
2024-08-21 21:41:24 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+  div_ = 0
+  if second == 0:
+    return 'Ошибка. Деление на ноль.'
+  else:
+    div_ = first / second
+    return div_
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(first, second):
+  div_ = 0
+  if second == 0:
+    return inf
+  else:
+    div_ = first / second
+    return div_
